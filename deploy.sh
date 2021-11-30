50bd5bcbfaa4b825ca92cd84d9df88ce1c5b04ea
JackHuo <cji4dk40@gmail.com>
2021-11-30 16:18:31 +0800

    fix: change dist git branch from master -> main

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-
+git branch -M main
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f https://github.com/<USERNAME>/<USERNAME>.GitHub.io.git main
 
